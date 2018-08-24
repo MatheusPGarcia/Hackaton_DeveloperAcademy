ed87130980783dab46ae148c4c3161b2900e9f8e
Unknown <matheuspgarcia@gmail.com>
2018-08-24 18:55:22 -0300

    Adding user fields

--- DeveloperAcademyHackaton/DeveloperAcademyHackaton/Model/User.swift
+++ DeveloperAcademyHackaton/DeveloperAcademyHackaton/Model/User.swift
@@ -10,4 +10,41 @@ import Foundation
 
 class User {
 
+    let userId: String
+    let name: String
+    let cpf: String
+    let userRg: String
+    // health Insurance
+    var appointments: [Appointment]
+    var saveDoctors: [Doctor]
+    var address: String
+    var doctorRatings: [DoctorRating]
+    var officeRatings: [OfficeRating]
+
+    init(_ userId: String, _ name: String, _ cpf: String, _ userRg: String, _ address: String) {
+
+        self.userId = userId
+        self.name = name
+        self.cpf = cpf
+        self.userRg = userRg
+        self.address = address
+
+        appointments = []
+        saveDoctors = []
+        doctorRatings = []
+        officeRatings = []
+    }
+
+    func defineUserAppointments(appointments: [Appointment]) {
+        self.appointments = appointments
+    }
+
+    func defineFavoritesDoctors(_ favorites: [Doctor]) {
+        self.saveDoctors = favorites
+    }
+
+    func defineRatings(forDoctors doctors: [DoctorRating], forOffice offices: [OfficeRating]) {
+        self.doctorRatings = doctors
+        self.officeRatings = offices
+    }
 }
