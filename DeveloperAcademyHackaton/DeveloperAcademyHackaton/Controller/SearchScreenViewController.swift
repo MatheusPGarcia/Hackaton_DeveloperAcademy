b1390aedafbaf2ebbb723f16db89b1316b838711
Unknown <matheuspgarcia@gmail.com>
2018-08-24 23:27:30 -0300

    Adding SearchScreen

---
+++ DeveloperAcademyHackaton/DeveloperAcademyHackaton/Controller/SearchScreenViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SearchScreenViewController.swift
+//  DeveloperAcademyHackaton
+//
+//  Created by Matheus Garcia on 24/08/18.
+//  Copyright © 2018 Matheus Garcia. All rights reserved.
+//
+
+import UIKit
+
+class SearchScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
